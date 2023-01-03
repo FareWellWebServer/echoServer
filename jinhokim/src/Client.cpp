5478a3db5a8fd4220ae6864a086957c2b011ea10
kimjinho1 <rlawlsgh8113@naver.com>
2023-01-04 02:07:08 +0900

    FEAT: Client 기본 구조 잡기

---
+++ jinhokim/src/Client.cpp
@@ -0,0 +1,9 @@
+#include "include/Client.hpp"
+
+Client::Client() {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Client::~Client() {
+    std::cout << "Default destructor called" << std::endl;
+}
