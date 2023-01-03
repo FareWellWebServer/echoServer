585f06652679b483fac7c69fc71e9a8f3c815976
kimjinho1 <rlawlsgh8113@naver.com>
2023-01-04 02:10:13 +0900

    FEAT: client_main 기본 구조 잡기

---
+++ jinhokim/client_main.cpp
@@ -0,0 +1,30 @@
+#include "include/Client.hpp"
+
+int main_process(int port) {
+    std::cout << port << std::endl;
+    return EXIT_SUCCESS;
+}
+
+int main(int ac, char** av) {
+    try {
+        if (ac < 2) {
+            std::cerr << "Argument error: few argument" << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        std::string port(av[1]);
+
+        for (char const& c: port) {
+            if (!std::isdigit(c)) {
+                std::cerr << "Argument error: port is not number" << std::endl;
+                return EXIT_FAILURE;
+            }
+        }
+
+        main_process(atoi(port.c_str()));
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+}
