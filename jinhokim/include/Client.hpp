1cf6ca7c09f5055ed2d5472cd5957b73f67901ce
kimjinho1 <rlawlsgh8113@naver.com>
2023-01-04 02:06:33 +0900

    FEAT: Client 기본 구조 잡기

---
+++ jinhokim/include/Client.hpp
@@ -0,0 +1,29 @@
+#ifndef CLIENT_HPP_
+# define CLIENT_HPP_
+
+# include <iostream>
+
+/**
+ * @brief 
+ * Client class, use socket communication for echo server
+ */
+class Client
+{
+    public:
+        /**
+         * @brief Construct a new Client object
+         * Default construtor
+         */
+        Client();
+        /**
+         * @brief Destroy the Client object
+         * Default destrutor
+         */
+        virtual ~Client();
+    private:
+
+};
+
+
+
+#endif  // CLIENT_HPP_
