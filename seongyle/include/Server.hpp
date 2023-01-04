89bac3432208b91b36de54e91634eddbe546beb5
yeonseong <yeonseong.dev@gmail.com>
2023-01-04 11:57:03 +0900

    FEAT: server 기본 구조

--- seongyle/include/Server.hpp
+++ seongyle/include/Server.hpp
@@ -1,6 +1,7 @@
 #ifndef SERVER_HPP
 # define SERVER_HPP
 
+# include <iostream>
 class Server
 {
 	public:
--- seongyle/server_main.cpp
+++ seongyle/server_main.cpp
@@ -1,6 +1,17 @@
-# include "../include/Server.hpp"
+#include "../include/Server.hpp"
 
-int	main(int ac, char** av)
-{
-	
+int	main_process() {	
+	Server server();
+}
+
+int main(int ac, char** av) {
+	try {
+		if (ac < 3) {
+		std::cerr << "please, input one argument(port)." << std::endl;
+		return EXIT_FAILURE;
+		}
+		main_process();
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
 }
