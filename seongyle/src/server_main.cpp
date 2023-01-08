414fdad17f07f5072bba65009ba4a35de0aec9b1
yeonseong <yeonseong.dev@gmail.com>
2023-01-09 00:36:42 +0900

    FEAT: add server action

--- seongyle/src/server_main.cpp
+++ seongyle/src/server_main.cpp
@@ -1,14 +1,15 @@
 #include "../include/Server.hpp"
 
-int	main_process() {	
-	Server server();
+int	main_process(const int& port) {	
+	Server server(port);
+	server.Run();
 }
 
 int main(int ac, char** av) {
 	try {
 		if (ac != 2)
 			throw std::runtime_error("please, input one argument(port).");
-		main_process();
+		main_process(atoi(av[1]));
 	} catch (const std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
