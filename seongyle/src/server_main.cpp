31029b135b4ef10153d23ad2af27bcc0533df062
yeonseong <yeonseong.dev@gmail.com>
2023-01-10 01:38:18 +0900

    CHORE: lint

--- seongyle/src/server_main.cpp
+++ seongyle/src/server_main.cpp
@@ -1,16 +1,15 @@
 #include "../include/Server.hpp"
 
-void	main_process(const int& port) {	
-	Server server(port);
-	server.Run();
+void main_process(const int& port) {
+  Server server(port);
+  server.Run();
 }
 
 int main(int ac, char** av) {
-	try {
-		if (ac != 2)
-			throw std::runtime_error("please, input one argument(port).");
-		main_process(atoi(av[1]));
-	} catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-	}
+  try {
+    if (ac != 2) throw std::runtime_error("please, input one argument(port).");
+    main_process(atoi(av[1]));
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
 }
