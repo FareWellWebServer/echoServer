7c2e3d8ca08929c4370ebbb7f788e0728335bfee
SihunLee-1016 <82656136+SihunLee-1016@users.noreply.github.com>
2023-01-10 18:11:50 +0900

    Revert "mulit client"

--- seongyle/src/client_main.cpp
+++ seongyle/src/client_main.cpp
@@ -1,17 +1,18 @@
 #include "../include/Client.hpp"
 
-void main_process(int port) {
-  Client client(port, "127.0.0.1");
-  client.Connect();
-  client.Run();
-  client.UnConnect();
+void	main_process(int port) {	
+	Client client(port, "127.0.0.1");
+	client.Connect();
+	client.Run();
+	client.UnConnect();
 }
 
 int main(int ac, char** av) {
-  try {
-    if (ac != 2) throw std::runtime_error("please, input one argument(port).");
-    main_process(atoi(av[1]));
-  } catch (const std::exception& e) {
-    std::cerr << e.what() << std::endl;
-  }
+	try {
+		if (ac != 2)
+			throw std::runtime_error("please, input one argument(port).");
+		main_process(atoi(av[1]));
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
 }
