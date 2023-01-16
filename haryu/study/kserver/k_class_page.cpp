142881bff9fc622b4cea56fed16365c5df8522d1
haryu <haryu@student.42seoul.kr>
2023-01-16 14:56:11 +0900

    CHORE : divide assignment (from cpp only to cpp plus hpp files)

--- haryu/study/kserver/k_class_page.cpp
+++ haryu/study/kserver/k_class_page.cpp
@@ -1,280 +1,226 @@
-#include <sys/types.h>
-#include <sys/event.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <fcntl.h>
-#include <stdlib.h> // atoi
-#include <unistd.h>
-
-#include <ctime>
-#include <iostream>
-#include <map>
-#include <vector>
-#include <string>
-#include <fstream>
-
-#define String std::string
-#define COUT std::cout 
-#define CEND std::endl
-#define CER std::cerr
-#define VEC std::vector
-#define MAP std::map
-
-#ifndef EVENT_MAX
-# define EVENT_MAX 100
-#endif
-
-#ifndef REUSE
-# define REUSE 1
-#endif 
-
-#ifndef LINGER
-# define LINGER 1
-#endif
-
-#ifndef DEFAULTLOCATION
-# define DEFAULTLLOCATION "/Users/haryu/workspace/echoServer/haryu/study/kserver/"
-#endif 
-
-#define HTTP "httpmsg"
-#define INDEX "index.html"
-#define INDEXC "index_comp.html"
-#define SRC "/src/"
+#include "k_class_page.hpp"
 
 // 목표 1 : 페이지 전달 서버 구축하기
 // 목표 2 : 다중 포트 상태로 값이 들어오면 서버 클래스가 자동으로 멀티 포트로 운영됨 
 // 목표 3 : 접근하면 index.html 전달하기 
 // 목표 4 : index_comp.html 전닳시키고 커낵션 종료 시켜보기
 
-struct host {
-	int serverSocket;
-	struct sockaddr_in serverAddr;
-};
+void Server::exitWithPerror(const String& msg_) {
+	CER << msg_ << CEND;
+	exit(EXIT_FAILURE);
+}
+void Server::changeEvents(VEC<struct kevent>& changeList_, uintptr_t ident, int16_t filter, uint16_t flags, uint16_t fflags, intptr_t data, void* udata) {
+	struct kevent tempEvent;
 
-class Server {
-private : 
-	int * serverSocket;
-	struct sockaddr_in * serverAddr;
-	int portnum;
-	int kq;
-	MAP<int, String> clients;
-	VEC<struct kevent> changeList;
-	struct kevent eventList[EVENT_MAX];
-	String msg;
+	EV_SET(&tempEvent, ident, filter,  flags, fflags, data, udata);
+	changeList_.push_back(tempEvent);
+}
+void Server::disconnectClients_(int clientFd, MAP<int, String>& clients_) {
+	std::cout << "client disconnected : " << clientFd << std::endl;
+	close(clientFd);
+	clients_.erase(clientFd);
+}
+Server::Server(int portNumber_, char **port_) : msg_("") {
+	if (portNumber_ == 1) { 
+		COUT << "./k_http_server {port 1} {port 2} ... " << CEND;
+		exit (1);
+	}
+	serverSocket_ = new int[portNumber_];
+	if(serverSocket_ == nullptr) {
+		CER << "constructor error" << CEND;
+		exit (1);
+	}
+	serverAddr_ = new sockaddr_in[portNumber_];
+	if (serverAddr_ == nullptr) {
+		CER << "constructor error" << CEND;
+		exit (1);
+	}
+	for (int i = 0; i + 1 < portNumber_; i++) {
+		serverSocket_[i] = socket(PF_INET, SOCK_STREAM, 0);
+		if (serverSocket_[i] == -1) { exitWithPerror("socket() error\n" + String(strerror(errno))); }
 
-	void exitWithPerror(const String& msg) {
-		CER << msg << CEND;
-		exit(EXIT_FAILURE);
+		memset(&serverAddr_[i], 0, sizeof(serverAddr_[i]));
+		serverAddr_[i].sin_family = AF_INET;
+		serverAddr_[i].sin_addr.s_addr = htonl(INADDR_ANY);
+		serverAddr_[i].sin_port = htons(atoi(port_[i + 1]));
 	}
-	void changeEvents(VEC<struct kevent>& changeList, uintptr_t ident, int16_t filter, uint16_t flags, uint16_t fflags, intptr_t data, void* udata) {
-		struct kevent tempEvent;
+	portnum_ = portNumber_ - 1;
+}
 
-		EV_SET(&tempEvent, ident, filter,  flags, fflags, data, udata);
-		changeList.push_back(tempEvent);
+Server::~Server() {
+	delete[] serverSocket_;
+	delete[] serverAddr_;
+}
+void Server::myBind() {
+	int ret;
+
+	for (int i = i; i < portnum_; i++) {
+		ret = bind(serverSocket_[i], reinterpret_cast<struct sockaddr *>(&serverAddr_[i]), sizeof(serverAddr_[i]));
+		if (ret == -1) { exitWithPerror("bind() error\n" + String(strerror(errno))); }
 	}
-	void disconnectClients(int clientFd, MAP<int, String>& clients) {
-		std::cout << "client disconnected : " << clientFd << std::endl;
-		close(clientFd);
-		clients.erase(clientFd);
+	return ;
+}
+
+void Server::myListen() {
+	int ret;
+
+	for (int i = 0; i < portnum_; i++) {
+		ret = listen(serverSocket_[i], 10);
+		if (ret == -1) { exitWithPerror("listen() error\n" +  String(strerror(errno))); }
+		fcntl(serverSocket_[i], F_SETFL, O_NONBLOCK);
 	}
-public :
-	Server(int portNumber, char **port) : msg("") {
-		if (portNumber == 1) { 
-			COUT << "./k_http_server {port 1} {port 2} ... " << CEND;
-			exit (1);
+}
+
+int	Server::mySetSockopt(int val , int fd) {
+	int ret;
+	
+	if (val == SO_REUSEADDR) {
+		int bf;
+
+		for (int i = 0; i < portnum_; i++) {
+			ret = setsockopt(serverSocket_[i], SOL_SOCKET, SO_REUSEADDR, &bf, sizeof(bf));
+			if (ret == -1) { exitWithPerror("setsockopt(SO_REUSERADDR) error\n" + String(strerror(errno))); }
+			COUT << "Socket(" << serverSocket_[i] << ")setting is succesed : SO_REUSEADDR" << CEND;
 		}
-		serverSocket = new int[portNumber];
-		if(serverSocket == nullptr) {
-			CER << "constructor error" << CEND;
-			exit (1);
-		}
-		serverAddr = new sockaddr_in[portNumber];
-		if (serverAddr == nullptr) {
-			CER << "constructor error" << CEND;
-			exit (1);
-		}
-		for (int i = 0; i + 1 < portNumber; i++) {
-			serverSocket[i] = socket(PF_INET, SOCK_STREAM, 0);
-			if (serverSocket[i] == -1) { exitWithPerror("socket() error\n" + String(strerror(errno))); }
-
-			memset(&serverAddr[i], 0, sizeof(serverAddr[i]));
-			serverAddr[i].sin_family = AF_INET;
-			serverAddr[i].sin_addr.s_addr = htonl(INADDR_ANY);
-			serverAddr[i].sin_port = htons(atoi(port[i + 1]));
-		}
-		portnum = portNumber - 1;
+		return (0);
 	}
-	~Server() {
-		delete[] serverSocket;
-		delete[] serverAddr;
-	}
-	void	myBind() {
-		int ret;
+	else if (val == SO_LINGER) {
+		struct linger loption_;
 
-		for (int i = i; i < portnum; i++) {
-			ret = bind(serverSocket[i], reinterpret_cast<struct sockaddr *>(&serverAddr[i]), sizeof(serverAddr[i]));
-			if (ret == -1) { exitWithPerror("bind() error\n" + String(strerror(errno))); }
-		}
-		return ;
-	}
-	void	myListen() {
-		int ret;
+		loption_.l_onoff = 1;
+		loption_.l_linger = 0;
 
-		for (int i = 0; i < portnum; i++) {
-			ret = listen(serverSocket[i], 10);
-			if (ret == -1) { exitWithPerror("listen() error\n" +  String(strerror(errno))); }
-			fcntl(serverSocket[i], F_SETFL, O_NONBLOCK);
-		}
+		ret = setsockopt(fd, SOL_SOCKET, SO_LINGER, &loption_, sizeof(loption_));
+		if (ret == -1) { exitWithPerror("setsockopt(SO_LINGER) error\n" + String(strerror(errno))); }
+		COUT << "Socket setting is succesed : SO_LINGER(" << fd <<  ")" << CEND;
+		return (0);
 	}
-	int		setSockopt(int val , int fd) {
-		int ret;
-		
-		if (val == SO_REUSEADDR) {
-			int bf;
+	else {
+		CER << "Socket setting is failed." << CEND;
+		return (-1);
+	}
+	
+}
 
-			for (int i = 0; i < portnum; i++) {
-				ret = setsockopt(serverSocket[i], SOL_SOCKET, SO_REUSEADDR, &bf, sizeof(bf));
-				if (ret == -1) { exitWithPerror("setsockopt(SO_REUSERADDR) error\n" + String(strerror(errno))); }
-				COUT << "Socket(" << serverSocket[i] << ")setting is succesed : SO_REUSEADDR" << CEND;
+bool	Server::IsServer(int fd) {
+	int ret = 0;
+
+	for (int i = 0; i < portnum_; i++) {
+		if (serverSocket_[i] == fd)
+			ret += 1 ;
+	}
+	return (ret);
+}
+
+void	Server::turnOn() {
+	kq_ = kqueue();
+	if (kq_ == -1) { exitWithPerror("kq_ueue() error\n" + String	(strerror(errno))); }
+
+	for (int i = 0; i < portnum_; i++) {
+		changeEvents(changeList_, serverSocket_[i], EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
+		COUT << "K-Page Server Started : " << serverSocket_[i] << CEND;
+	}
+
+	int newEvents_;
+	struct kevent * currEvent_;
+	while(true) {
+		newEvents_ = kevent(kq_, &changeList_[0], changeList_.size(), eventList_, EVENT_MAX, nullptr);
+		if (newEvents_ == -1) { exitWithPerror("kevent() error\n" + String(strerror(errno))); }
+
+		changeList_.clear();
+
+		for (int i = 0; i < newEvents_; ++i) {
+			currEvent_ = &eventList_[i];
+			if (currEvent_->flags & EV_ERROR) {
+				for (int i = 0; i < portnum_; i ++) {
+					if (currEvent_->ident == serverSocket_[i])
+						exitWithPerror("server socket error");
+					else {
+						CER << "clients_ socket error" << CEND;
+						disconnectClients_(currEvent_->ident, clients_); 
+					}
+				}
 			}
-			return (0);
-		}
-		else if (val == SO_LINGER) {
-			struct linger loption;
+			else if (currEvent_->filter == EVFILT_READ) {
+				if (IsServer(currEvent_->ident)) {
+					/* Accept new client */
+					int clientS_ocket(accept(currEvent_->ident, NULL, NULL));
 
-			loption.l_onoff = 1;
-			loption.l_linger = 0;
+					if (clientS_ocket == -1) { exitWithPerror("accept() error\n" + String(strerror(errno))); }
+					COUT << "accept new client : " << clientS_ocket << CEND;
+					fcntl(clientS_ocket, F_SETFL, O_NONBLOCK);
 
-			ret = setsockopt(fd, SOL_SOCKET, SO_LINGER, &loption, sizeof(loption));
-			if (ret == -1) { exitWithPerror("setsockopt(SO_LINGER) error\n" + String(strerror(errno))); }
-			COUT << "Socket setting is succesed : SO_LINGER(" << fd <<  ")" << CEND;
-			return (0);
-		}
-		else {
-			CER << "Socket setting is failed." << CEND;
-			return (-1);
-		}
-		
-	}
-	bool	IsServer(int fd) {
-		int ret = 0;
-
-		for (int i = 0; i < portnum; i++) {
-			if (serverSocket[i] == fd)
-				ret += 1 ;
-		}
-		return (ret);
-	}
-	void	turnOn() {
-		kq = kqueue();
-		if (kq == -1) { exitWithPerror("kqueue() error\n" + String	(strerror(errno))); }
-
-		for (int i = 0; i < portnum; i++) {
-			changeEvents(changeList, serverSocket[i], EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
-			COUT << "K-Page Server Started : " << serverSocket[i] << CEND;
-		}
-		
-
-		int newEvents;
-		struct kevent * currEvent;
-		while(true) {
-			newEvents = kevent(kq, &changeList[0], changeList.size(), eventList, EVENT_MAX, nullptr);
-			if (newEvents == -1) { exitWithPerror("kevent() error\n" + String(strerror(errno))); }
-
-			changeList.clear();
-
-			for (int i = 0; i < newEvents; ++i) {
-				currEvent = &eventList[i];
-				if (currEvent->flags & EV_ERROR) {
-					for (int i = 0; i < portnum; i ++) {
-						if (currEvent->ident == serverSocket[i])
-							exitWithPerror("server socket error");
-						else {
-							CER << "clients socket error" << CEND;
-							disconnectClients(currEvent->ident, clients); 
-						}
-					}
+					/* add event for client socket - add read && write event */
+					changeEvents(changeList_, clientS_ocket, EVFILT_READ, EV_ADD | EV_ENABLE | EV_EOF, 0, 0, NULL);
+					changeEvents(changeList_, clientS_ocket, EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, NULL);
+					mySetSockopt(SO_LINGER,  clientS_ocket);
+					clients_[clientS_ocket] = "";
 				}
-				else if (currEvent->filter == EVFILT_READ) {
-					if (IsServer(currEvent->ident)) {
-						/* Accept new client */
-						int clientSocket(accept(currEvent->ident, NULL, NULL));
-						if (clientSocket == -1) { exitWithPerror("accept() error\n" + String(strerror(errno))); }
-						COUT << "accept new client : " << clientSocket << CEND;
-						fcntl(clientSocket, F_SETFL, O_NONBLOCK);
-
-						/* add event for client socket - add read && write event */
-						changeEvents(changeList, clientSocket, EVFILT_READ, EV_ADD | EV_ENABLE | EV_EOF, 0, 0, NULL);
-						changeEvents(changeList, clientSocket, EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, NULL);
-						setSockopt(SO_LINGER,  clientSocket);
-						clients[clientSocket] = "";
-					}
-					else if (clients.find(currEvent->ident) != clients.end()) {
-						/* read data from clients */
-						char buf[1024];
-						int n = recv(currEvent->ident, buf, sizeof(buf), 0);
-						buf[n] = '\0';
-						clients[currEvent->ident].append(buf);
-						COUT << "received data from " << currEvent->ident << ":\n" << clients[currEvent->ident] << CEND;
-					}
+				else if (clients_.find(currEvent_->ident) != clients_.end()) {
+					/* read data from clients_ */
+					char buf[1024];
+					int n = recv(currEvent_->ident, buf, sizeof(buf), 0);
+					buf[n] = '\0';
+					clients_[currEvent_->ident].append(buf);
+					COUT << "received data from " << currEvent_->ident << ":\n" << clients_[currEvent_->ident] << CEND;
 				}
-				else if (currEvent->filter == EVFILT_WRITE) {
-					/* send Data to client*/
-					std::map<int, String>::iterator it = clients.find(currEvent->ident);
-					if (it != clients.end()) {
-						int n;
-						n = send(currEvent->ident, msg.c_str(), strlen(msg.c_str()), MSG_DONTWAIT);
-						clients[currEvent->ident].clear();
-						disconnectClients(currEvent->ident, clients);
-					}
+			}
+			else if (currEvent_->filter == EVFILT_WRITE) {
+				/* send Data to client*/
+				std::map<int, String>::iterator it = clients_.find(currEvent_->ident);
+				if (it != clients_.end()) {
+					int n;
+					n = send(currEvent_->ident, msg_.c_str(), strlen(msg_.c_str()), MSG_DONTWAIT);
+					clients_[currEvent_->ident].clear();
+					disconnectClients_(currEvent_->ident, clients_);
 				}
 			}
 		}
 	}
-	void	showYourSelf() {
-		for (int i = 0; i < portnum; i++) {
-			COUT << "Server Socket : " << this->serverSocket[i] << CEND;
-			COUT << "Server Port : " << ntohs(this->serverAddr[i].sin_port) << CEND;
-		}
+}
+void	Server::showYourSelf() {
+	for (int i = 0; i < portnum_; i++) {
+		COUT << "Server Socket : " << this->serverSocket_[i] << CEND;
+		COUT << "Server Port : " << ntohs(this->serverAddr_[i].sin_port) << CEND;
 	}
+}
 
-	void	fileSetting(const char * path) {
-		std::ifstream httpmsg;
-		std::ifstream entity;
-		String entityloc(DEFAULTLLOCATION);
+void	Server::fileSetting(const char * path) {
+	std::ifstream httpmsg_;
+	std::ifstream entity;
+	String entityloc(DEFAULTLLOCATION);
 
-		httpmsg.open(String(DEFAULTLLOCATION).append(HTTP), std::ifstream::in);
-		entityloc.append(path);
+	httpmsg_.open(String(DEFAULTLLOCATION).append(HTTP), std::ifstream::in);
+	entityloc.append(path);
 
-		COUT << entityloc << CEND;
+	COUT << entityloc << CEND;
 
-		entity.open(entityloc.c_str(), std::ifstream::in);
+	entity.open(entityloc.c_str(), std::ifstream::in);
 
-		int lengthFirst;
-		int lengthSecond;
+	int lengthFirst;
+	int lengthSecond;
 
-		httpmsg.seekg(0, httpmsg.end);
-		lengthFirst = httpmsg.tellg();
-		httpmsg.seekg(0, httpmsg.beg);
+	httpmsg_.seekg(0, httpmsg_.end);
+	lengthFirst = httpmsg_.tellg();
+	httpmsg_.seekg(0, httpmsg_.beg);
 
-		entity.seekg(0, entity.end);
-		lengthSecond = entity.tellg();
-		entity.seekg(0, entity.beg);
+	entity.seekg(0, entity.end);
+	lengthSecond = entity.tellg();
+	entity.seekg(0, entity.beg);
 
-		char * buffer1 = new char[lengthFirst + 1];
-		char * buffer2 = new char[lengthSecond + 1];
-		buffer1[lengthFirst] = '\0';
-		buffer2[lengthSecond] = '\0';
+	char * buffer1 = new char[lengthFirst + 1];
+	char * buffer2 = new char[lengthSecond + 1];
+	buffer1[lengthFirst] = '\0';
+	buffer2[lengthSecond] = '\0';
 
-		httpmsg.read(buffer1, lengthFirst);
-		entity.read(buffer2, lengthSecond);
+	httpmsg_.read(buffer1, lengthFirst);
+	entity.read(buffer2, lengthSecond);
 
-		msg.append(buffer1);
-		msg.push_back(static_cast<char>(13));
-		msg.append("\n");
-		msg.append(buffer2);
-	}
-};
+	msg_.append(buffer1);
+	msg_.push_back(static_cast<char>(13));
+	msg_.append("\n");
+	msg_.append(buffer2);
+}
 
 int main(int ac, char **av) {
 	Server myServer(ac, av);
@@ -282,7 +228,7 @@ int main(int ac, char **av) {
 	myServer.myBind();
 	myServer.myListen();
 	myServer.showYourSelf();
-	myServer.setSockopt(SO_REUSEADDR, 0);
+	myServer.mySetSockopt(SO_REUSEADDR, 0);
 	myServer.fileSetting(INDEX);
 	myServer.turnOn();
 	
