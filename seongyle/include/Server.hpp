d9d3ea2f231c676dedbd0e9adbfa174531fec58a
yeonseong <yeonseong.dev@gmail.com>
2023-01-18 04:15:41 +0900

    WIP: multi port

--- seongyle/include/Server.hpp
+++ seongyle/include/Server.hpp
@@ -11,25 +11,30 @@
 #include <iostream>
 #include <vector>
 
+enum event {
+  SIZE = 100,
+};
+
 class Server {
  public:
   explicit Server(const int& port);
   virtual ~Server(void);
   void Run();
+  void AddPort(const int port);
 
  private:
   const static int BACKLOG = 10;
   const static int BUFFER_SIZE = 1024;
   int socket_option_;
-  int server_fd_;
-  sockaddr_in server_addr_;
+  std::vector<int> server_fds_;
+  std::vector<sockaddr_in> server_addrs_;
   struct kevent event_;
   int kq_;
   std::vector<int> clients_;
   /// @brief Accept the client, and return client fd
   /// @param  void
   /// @return client fd
-  void Accept(void);
+  void Accept(const int idx);
   int Recv(int& client_fd, char* buffer, int flags);
   void Send(int& client_fd, char* buffer, int flags, int bytes_received);
   void Bind(void);
